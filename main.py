8e9ea9a5e40be1d59ca6a4ef8d5f80eab5ef86a2
AtefRender <127673114+AtefRender@users.noreply.github.com>
2023-03-13 00:52:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup as bs
 import requests
 import json
 from server import server
+import time
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
