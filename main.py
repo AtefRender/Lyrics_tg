0a68c1f83a1a43874ab3455e0a0b58f1da4112d8
AtefRender <127673114+AtefRender@users.noreply.github.com>
2023-03-13 12:29:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import time
 
 headers = {
     'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36',
-    'Connection':'close',
+    'Connection': 'close',
     }
 
 API_KEY = os.environ.get('API_KEY')
